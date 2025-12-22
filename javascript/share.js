45d22a2bd79139b8945542b5918a011da92d8b9d
蘇優琳 <young30030prc@gmail.com>
2025-12-23 00:48:30 +0800

    Update share.js

--- javascript/share.js
+++ javascript/share.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
             var currentUrl = encodeURIComponent(window.location.href);
             var currentTitle = encodeURIComponent(document.title);
             var shareUrl = 'https://twitter.com/intent/tweet?text=' + currentTitle + '&url=' + currentUrl;
-            window.open(shareUrl, '_blank', 'width=600,height=400');
+            window.open(shareUrl, '_blank', 'width=800,height=600');
             return;
         }
         var facebookBtn = e.target.closest('#share-facebook');
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
             var currentUrl = encodeURIComponent(window.location.href);
             var shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + currentUrl;
             
-            window.open(shareUrl, '_blank', 'width=600,height=400');
+            window.open(shareUrl, '_blank', 'width=800,height=600');
         }
     });
 });
