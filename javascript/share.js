00bdaddac9ec0aa70ddc3ee9bc5344626d95bf14
蘇優琳 <young30030prc@gmail.com>
2025-12-23 00:41:13 +0800

    Update share.js

--- javascript/share.js
+++ javascript/share.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var twitterBtn = document.getElementById('share-twitter');
-  var facebookBtn = document.getElementById('share-facebook');
-
-  twitterBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    var currentUrl = encodeURIComponent(window.location.href);
-    var currentTitle = encodeURIComponent(document.title);
-    var shareUrl = 'https://twitter.com/intent/tweet?text=' + currentTitle + '&url=' + currentUrl;
-    window.open(shareUrl, '_blank', 'width=600,height=400');
-  });
-
-  facebookBtn.addEventListener('click', function(e) {
-    e.preventDefault();
-    var currentUrl = encodeURIComponent(window.location.href);
-    var shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + currentUrl;
-    window.open(shareUrl, '_blank', 'width=600,height=400');
-  });
+    document.body.addEventListener('click', function(e) {
+        var twitterBtn = e.target.closest('#share-twitter');
+        
+        if (twitterBtn) {
+            e.preventDefault();
+            var currentUrl = encodeURIComponent(window.location.href);
+            var currentTitle = encodeURIComponent(document.title);
+            var shareUrl = 'https://twitter.com/intent/tweet?text=' + currentTitle + '&url=' + currentUrl;
+            window.open(shareUrl, '_blank', 'width=600,height=400');
+            return;
+        }
+        var facebookBtn = e.target.closest('#share-facebook');
+        
+        if (facebookBtn) {
+            e.preventDefault();
+            
+            var currentUrl = encodeURIComponent(window.location.href);
+            var shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + currentUrl;
+            
+            window.open(shareUrl, '_blank', 'width=600,height=400');
+        }
+    });
 });
